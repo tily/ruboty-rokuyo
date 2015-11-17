ef0a9b6f1186f0b13e9b23d635f65602a63ab9f6
tily <tidnlyam@gmail.com>
2015-11-17 18:58:58 +0900

    add rokuyo description

--- lib/ruboty/handlers/rokuyo.rb
+++ lib/ruboty/handlers/rokuyo.rb
@@ -1,9 +1,16 @@
+# coding:utf-8
 require 'qreki'
 require 'time'
+require 'json'
 
 module Ruboty
   module Handlers
     class Rokuyo < Base
+      def initialize(robot)
+        super(robot)
+        @config = JSON.parse File.read(File.dirname(__FILE__) + '/rokuyo.json')
+      end
+
       on(
         /rokuyo( (?<date>.+))?/,
         name: 'rokuyo',
@@ -13,7 +20,16 @@ module Ruboty
       def rokuyo(message)
         time = message[:date] ? Time.parse(message[:date]) : Time.now
         rokuyo = Qreki.calc(time.year, time.month, time.day).rokuyou
-        message.reply rokuyo
+        message.reply "☆今日の六曜\☆\n\n"
+        message.reply "  ===="
+        message.reply "  #{rokuyo}"
+        message.reply "  ===="
+        message.reply description(rokuyo).gsub(/^/, "  ")
+      end
+
+      def description(name)
+        rokuyo = @config.find{|rokuyo| rokuyo['rokuyo'] == name }
+        rokuyo['description']
       end
     end
   end
